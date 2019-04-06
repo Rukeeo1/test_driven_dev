446a912d96e91379042974237afc2f67288c0cbb
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-04-06 22:10:16 +0100

    initial commit

---
+++ add.js
@@ -0,0 +1,15 @@
+
+// var ruk = "we are loving this";
+// console.log(module.exports['rukee'] = 'kkkk');
+// console.log(module.exports['seyi'] = 'we love this');
+// console.log(module)
+
+const myObj = {
+    add: function(num1,num2){
+        return num1 + num2;
+    }
+}
+
+module.exports = myObj;
+console.log(module)
+
---
+++ add.test.js
@@ -0,0 +1,11 @@
+const add = require('./add.js');
+
+test('adds 2 + 2 to equal 4', () => {
+    //first add is the file name (add.js)..forgive my crazy syntax
+    expect(add.add(2,2)).toBe(4);
+});
+
+test('adds 2 + 2 to NOT equal 5', ()=>{
+    //negating the test
+    expect(add.add(2,2)).not.toBe(5);
+});
